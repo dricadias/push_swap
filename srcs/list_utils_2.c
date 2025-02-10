d8d5a639c03aed75c5387dd429893cc17aac6de3
dricadias <89052954+dricadias@users.noreply.github.com>
2025-02-10 19:50:47 +0000

    rename files

--- srcs/list_utils_2.c
+++ srcs/list_utils_2.c
@@ -1,26 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lists_utils_2.c                                    :+:      :+:    :+:   */
+/*   list_utils_2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:10:37 by adias-do          #+#    #+#             */
-/*   Updated: 2025/02/05 15:12:06 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/02/10 13:04:16 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void	lstclear(t_stack **stack, void (*del)(void *))
+#include "../includes/push_swap.h"
+
+void	ft_stackfree(t_stack **stack)
 {
 	t_stack	*temp;
 
-	if (!*stack)
+	if (!stack || !(*stack))
 		return ;
 	while (*stack)
 	{
 		temp = (*stack)->next;
-		ft_lstdelone(*stack, del);
+		(*stack)->nbr = 0;
+		free(*stack);
 		*stack = temp;
 	}
-	*stack = NULL;
-}*/ // nao vou usar
+} // clear stacks
+
+void	ft_freestr(char **str)
+{
+	char	*temp;
+
+	if (!str)
+		return ;
+	while (*str)
+	{
+		temp = *str;
+		str++;
+		free(temp);
+	}
+	*str = NULL;
+} // clear strings
