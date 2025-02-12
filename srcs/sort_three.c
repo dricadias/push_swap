1854492de65af84e4bf337cdd37c1639109febfa
dricadias <89052954+dricadias@users.noreply.github.com>
2025-02-12 18:51:31 +0000

    renamed and sorting three args

--- srcs/sort_three.c
+++ srcs/sort_three.c
@@ -14,16 +14,38 @@
 
 void	sort_three(t_stack **stack_a)
 {
-	t_stack	*first_a;
-	t_stack	*second_a;
-	t_stack	*third_a;
+	static t_stack	*first;
+	static t_stack	*sec;
+	static t_stack	*thrd;
 
 	if (!stack_a || !(*stack_a) || !((*stack_a)->next))
 		return ;
-	second_a = first_a->next;
-	third_a = second_a->next;
-	if (!check_sorted(*stack_a, 0))
+	first = *stack_a;
+	sec = first->next;
+	thrd = sec->next;
+	if (!check_sorted(*stack_a, 0) && lstsize(*stack_a) == 3)
 	{
-		if (first_a < second_a)
+		if (first->nbr < sec->nbr && first->nbr < thrd->nbr)
+		{
+			ft_reverse_rotate(stack_a, 0, 0);
+			ft_swap_a(stack_a, 0);
+		}
+		else if (first->nbr > sec->nbr && sec->nbr > thrd->nbr)
+		{
+			ft_rotate(stack_a, 0, 0);
+			ft_swap_a(stack_a, 0);
+		}
+		else
+			one_move(stack_a, first, sec, thrd);
 	}
+}
+
+void	one_move(t_stack **stack_a, t_stack *first, t_stack *sec, t_stack *thrd)
+{
+	if (first->nbr > sec->nbr && first->nbr > thrd->nbr)
+		ft_rotate(stack_a, 0, 0);
+	else if (first->nbr > sec->nbr && first->nbr < thrd->nbr)
+		ft_swap_a(stack_a, 0);
+	else
+		ft_reverse_rotate(stack_a, 0, 0);
 }
