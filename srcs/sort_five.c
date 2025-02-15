8f313aa98f5ebbbe6a8a1fe13de37c22aa51c826
dricadias <89052954+dricadias@users.noreply.github.com>
2025-02-15 20:49:51 +0000

    finished sort four (seems to work til now) and starting sort five

--- srcs/sort_five.c
+++ srcs/sort_five.c
@@ -12,7 +12,42 @@
 
 #include "../includes/push_swap.h"
 
-void	sort_five(t_stack *stack_a, t_stack *stack_b)
+void	sort_four(t_stack **stack_a)
 {
+	t_stack		*stack_b;
+	int			index_min;
 
+	if (!stack_a || !*stack_a || !((*stack_a)->next))
+		return ;
+	stack_b = NULL;
+	index_min = ft_index(*stack_a, ft_min(*stack_a));
+	if (index_min == 0)
+		ft_push(stack_a, &stack_b, 0, 1);
+	else if (index_min == 1)
+		ft_swap_a(stack_a, 0);
+	else if (index_min == 2)
+	{
+		ft_reverse_rotate(stack_a, 0, 0);
+		ft_reverse_rotate(stack_a, 0, 0);
+	}
+	else
+		ft_reverse_rotate(stack_a, 0, 0);
+	if (index_min)
+		ft_push(stack_a, &stack_b, 0, 1);
+	sort_three(stack_a);
+	ft_push(&stack_b, stack_a, 0, 0);
+}
+
+void	sort_five(t_stack **stack_a)
+{
+	t_stack		*stack_b;
+	int			index_min;
+
+	if (!stack_a || !*stack_a || !((*stack_a)->next))
+		return ;
+	stack_b = NULL;
+	ft_push(stack_a, &stack_b, 0, 1);
+	ft_push(stack_a, &stack_b, 0, 1);
+	if (check_sorted(stack_b, 1))
+		ft_swap_b(&stack_b, 0);
 }
